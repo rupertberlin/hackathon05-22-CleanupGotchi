e11a740d9e702786fb0aa3c0a8e755a9530ca2d5
puokl <phuoc@sapo.pt>
2022-05-12 14:48:24 +0200

    smallchange

--- frontend/src/components/Food/Food.jsx
+++ frontend/src/components/Food/Food.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 
 export default function Food() {
+  const url = axios
+    .get(url)
+    .then((response) => {
+      setData(response.data);
+
+      console.log("data is: ", data);
+    })
+    .catch((err) => console.log("erorr ", err));
   return <div></div>;
 }
