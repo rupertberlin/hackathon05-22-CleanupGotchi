f4e4acc62ea92d48079117183323dac8164d2972
PIERRE AHSATAL <asathal.pierre@gmail.com>
2022-05-12 16:16:58 +0200

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import ControlledAccordions from "./components/Accordion";
+import Food from './components/Food';
 import "./App.css";
 
 const App = () => {
   return (
     <div className="accordion">
       <ControlledAccordions />
-
+      <Food />
       <ControlledAccordions />
     </div>
   );
