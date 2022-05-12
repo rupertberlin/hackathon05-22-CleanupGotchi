fb71d5f4a2baf0ed48830e07fb3bf8b3eed0e2f7
codeus-ex-machina <thegreatjordan@gmx.de>
2022-05-12 21:41:12 +0200

    Add selection of food to central state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
+import React from "react";
 import ControlledAccordions from "./components/Accordion";
 import Food from "./components/Food";
 import "./App.css";
+import { flexbox } from "@mui/system";
 
 const App = () => {
+  // In this object, all choices we make will be stored  (incl. the corresponding happiness values / emissions)
+  const [allConsumption, setAllConsumption] = React.useState({
+    // Each value should have the form of [ITEM NAME, HAPPINESS POINTS, DAILY EMISSIONS]
+    breakfast: [],
+    lunch: [],
+    dinner: [],
+  });
+
   return (
-    <div className="accordion">
-      <ControlledAccordions />
-      <Food />
-      <ControlledAccordions />
-    </div>
+    <>
+      <header>Green Gotchi</header>
+      <div></div>
+      <div id="allBelowHeaderContainer">
+        <div className="accordion">
+          <ControlledAccordions />
+        </div>
+        <div id="containerForCentralDivs">
+          <div id="containerForColumnWithHappinessProgress">
+            HAPPINESS SECTION
+          </div>
+          <div id="containerForColumnWithPicture">PICTURE SECTION</div>
+          <div id="containerForColumnWithTotalEmissions">
+            TOTAL EMISSIONS {allConsumption.breakfast[0]}
+          </div>
+        </div>
+      </div>
+
+      <Food
+        allConsumption={allConsumption}
+        setAllConsumption={setAllConsumption}
+      />
+    </>
   );
 };
 
--- src/components/Food.js
+++ src/components/Food.js
@@ -2,16 +2,27 @@ import React from "react";
 
 import FoodData from "./Food.json";
 
-const Food = () => {
+const Food = ({ allConsumption, setAllConsumption }) => {
   return (
     <div>
       {FoodData.map((snackData) => {
         return (
           <div key={snackData.id}>
-            <strong>This food :</strong>
-            {snackData.Name} <strong>use </strong>
-            {snackData.Impact} <strong>CO2 </strong>
-            <button>Select this food</button>
+            {snackData.Name} --- 1kg <strong>causes </strong> {snackData.Impact}
+            of CO2{" "}
+            <button
+              onClick={() => {
+                setAllConsumption(
+                  // Here, we create a new Object to replace the current one.
+                  //Create an empty {}, merge the current object into it, then merge an Object that includes the key/value we want to change.
+                  Object.assign({}, allConsumption, {
+                    breakfast: [snackData.Name, 5, snackData.Impact],
+                  })
+                );
+              }}
+            >
+              Select this food
+            </button>
           </div>
         );
       })}
