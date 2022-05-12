e32f7a136cfe2d7f4abfd43086b7b0fef465e819
rupertberlin <ulrich.laitenberger@googlemail.com>
2022-05-12 19:24:19 +0200

    gamecontainer and gamplay component added

---
+++ src/components/Gameplay.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ChooseGotchi from "./ChooseGotchi";
+
+export default function Gameplay(props){
+    return(
+      <h3>Character: {props.gotchiName}</h3>  
+    );
+}
