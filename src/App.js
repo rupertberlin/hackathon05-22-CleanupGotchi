279e48166cf28e8e0d82d154a7c5b28e49641c63
rupertberlin <ulrich.laitenberger@googlemail.com>
2022-05-12 18:53:13 +0200

    settet up character choice

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import ChooseGotchi from "./components/ChooseGotchi";
+
 import ControlledAccordions from "./components/Accordion";
 import Food from "./components/Food";
 import "./App.css";
 
 const App = () => {
+  const [characterName, setCharacterName] = useState("");
+
   return (
-    <div className="accordion">
-      <ControlledAccordions />
-      <Food />
-      <ControlledAccordions />
+    <div className="">
+      <Router>
+        <Routes>
+          <Route path="/" element={characterName==="" ?<Navigate to="/choose-gotchi" />:"" }/>
+          <Route
+            path="/choose-gotchi"
+            element={<ChooseGotchi setCharacter={setCharacterName} />}
+          />
+        </Routes>
+      </Router>
     </div>
   );
 };
