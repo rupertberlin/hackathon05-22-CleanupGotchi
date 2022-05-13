03eff4226217b72c915f435345c85e904f2ad797
PIERRE AHSATAL <asathal.pierre@gmail.com>
2022-05-13 09:09:30 +0200

    updated databases with satisfation and cost values

--- src/components/Contexts/CostContext.js
+++ src/components/Contexts/CostContext.js
@@ -1,6 +1,6 @@
 import Context from "@mui/base/TabsUnstyled/TabsContext";
 import { createContext } from "react";
 
-const CostContext = createContext();
+const CostContext = createContext({money:1000, satisfaction:0});
 
 export default CostContext;
